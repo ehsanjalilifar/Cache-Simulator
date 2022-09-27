229abdfc4f41f747013498bae4e0325d0cb2b78e
ehsanjalilifar <ehsan.jalilifar@gmail.com>
2022-09-27 18:40:30 -0500

    Dynamic cache was defined

--- CacheSimulator/main.cpp
+++ CacheSimulator/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#define KB 1024
 
 int main(int argc, const char * argv[]) {
     
@@ -14,10 +15,19 @@ int main(int argc, const char * argv[]) {
     int blocksize = (int) *argv[3]; // The size of a single cache block in bytes
     char repl = *argv[4]; // The replacement policy; 'l' means LRU, 'r' means random.
     
+    int n_sets = nk * KB / assoc / blocksize;
+    
     for (int i = 1; i < argc; i++) {
         std::cout << argv[i] << std::endl;
     }
-    std::cout << "Hello, World!\n";
+    
+    std::cout << "number of sets = " << n_sets << std::endl;
+    
+    // Define a 2-D array to store the addresses in the cach
+    unsigned long long ** cache = new unsigned long long*[n_sets];
+    for (int i = 0; i < n_sets; i++) {
+        cache[i] = new unsigned long long[assoc];
+    }
     
 //    char type;
 //    unsigned long long address;

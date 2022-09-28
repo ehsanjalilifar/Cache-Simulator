97ac93425726afecc6f8ee20da02fe23d448cf3e
ehsanjalilifar <ehsan.jalilifar@gmail.com>
2022-09-27 20:38:23 -0500

    Fixed bug in CL arguments

--- CacheSimulator/main.cpp
+++ CacheSimulator/main.cpp
@@ -6,42 +6,55 @@
 //
 
 #include <iostream>
+#include <fstream>
 #define KB 1024
 
 int main(int argc, const char * argv[]) {
     
-    int nk = (int) *argv[1]; // The capacity of the cache in kilobytes
-    int assoc = (int) *argv[2]; // The associativity of the cache
-    int blocksize = (int) *argv[3]; // The size of a single cache block in bytes
+    int nk = std::atoi(argv[1]); // The capacity of the cache in kilobytes
+    int assoc = std::atoi(argv[2]); // The associativity of the cache
+    int blocksize = std::atoi(argv[3]); // The size of a single cache block in bytes
     char repl = *argv[4]; // The replacement policy; 'l' means LRU, 'r' means random.
-    
+
     int n_sets = nk * KB / assoc / blocksize;
-    
-    for (int i = 1; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
-    }
     
     std::cout << "number of sets = " << n_sets << std::endl;
     
-    // Define a 2-D array to store the addresses in the cach
-    unsigned long long ** cache = new unsigned long long*[n_sets];
+
+//     Define a 2-D array to store the addresses in the cach
+    unsigned long long** cache = new unsigned long long*[n_sets];
+    bool** valid = new bool*[n_sets]; // defines the valid bit
     for (int i = 0; i < n_sets; i++) {
         cache[i] = new unsigned long long[assoc];
+        valid[i] = new bool[assoc];
     }
+
+    char operation;
+    int address;
     
+    std::ifstream infile;
+    infile.open("input.txt", std::ios::in);
+    std::cout << infile.is_open() << std::endl;
+    while (infile.is_open()) {
+        std::cin >> operation >> std::hex >> address;
+//        std::cout << "address = " << address << std::endl;
+        std::cout << "ridi" << std::endl;
+    }
+
+
+    printf("ridi");
+    
+//     Cache miss rate calculations
     unsigned long long r_miss, w_miss, n_read, n_write;
     r_miss = w_miss = n_read = n_write = 0;
-    
-    // Miss calculations
+
     double miss_rate, r_miss_rate, w_miss_rate;
     miss_rate = (double) (r_miss + w_miss) / (n_read + n_write); // total miss rate
     r_miss_rate = (double) r_miss / n_read; // read miss rate
     w_miss_rate = (double) w_miss / n_read; // write miss rate
-    
+
     std::cout << r_miss + w_miss << " " << miss_rate << " " << r_miss << " " << r_miss_rate << " " << w_miss << " " << w_miss_rate << std::endl;
     
-//    char type;
-//    unsigned long long address;
 //    std::cin >> type >> address;
     
     return 0;
